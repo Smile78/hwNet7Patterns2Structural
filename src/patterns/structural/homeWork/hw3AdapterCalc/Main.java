ea9e1ad69d2ccb9f315e39ebfec7b75cd6e1a4b8
Smile78 <dronussr@gmail.com>
2022-04-09 17:38:25 +0300

    hw3 minds 2

--- src/patterns/structural/homeWork/hw3AdapterCalc/Main.java
+++ src/patterns/structural/homeWork/hw3AdapterCalc/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-         Calculator calc = new Calculator();
+        Calculator calc = new Calculator();
         System.out.println(
                 calc.newFormula()
                         .addOperand(5)
--- src/patterns/structural/homeWork/hw4FacadeBin/Main.java
+++ src/patterns/structural/homeWork/hw4FacadeBin/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BinOps bins = new BinOps();
 
+        //какой то обычный статик класс получился
         System.out.println(bins.sum("01011101","01101101"));
         System.out.println(bins.mult("01011101","01101101"));
     }
